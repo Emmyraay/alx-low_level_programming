c39762dca830586e9692d14d8620f916e71a4609
Emmyraay <ibrahim.emmy@yahoo.com>
2022-07-14 12:24:23 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* function main is where the code starts*/
+/**
+ * main - returns void
+ * on completion
+ *
+ * Return: 0 at the compltion of the task
+ */
 int main(void)
 {
 	int n;
@@ -16,7 +21,7 @@ int main(void)
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-*(p + 5) = 98;
+	*(p + 5) = 98;
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
