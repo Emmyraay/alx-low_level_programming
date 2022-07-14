20e2fb20871b14f45ad505788b6405b04a12575b
Emmyraay <ibrahim.emmy@yahoo.com>
2022-07-14 05:18:56 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int destLen = 0;
+	int srcLen = 0;
+
+	while (dest[destLen++])
+		;
+	while (src[srcLen])
+	{
+		dest[destLen] = src[srcLen];
+		srcLen++;
+		destLen++;
+	}
+	dest[destLen] = '\0';
+	return (dest);
+}
